53424bc0bb5cb41f469d9096406267659600dff7
MYSOFT\wangj38 <wangj38@mingyuanyun.com>
2021-08-30 11:34:45 +0800

    TIJIAO funcA

--- mmm.py
+++ mmm.py
@@ -7,4 +7,13 @@ def test02(a,b):
     return a-b
 
 def test03(a,b):
-    return a*b
+    return a*b    return a*b
+
+class funcA:
+    
+    
+    def testA(self,a,b):
+        return a*b
+    
+    def testb(self,c,d):
+        return c*d
